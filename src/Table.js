6cb5f84bebcb87f0a49036b5efa6a4072371e1ed
faisala <alquaddoomi@nexus.ethz.ch>
2019-12-03 16:09:55 +0100

    Applied React-PropTypes-to-prop-types codemod

--- src/Table.js
+++ src/Table.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 let simpleGet = key => data => data[key];
 let keyGetter = keys => data => keys.map(key => data[key]);
